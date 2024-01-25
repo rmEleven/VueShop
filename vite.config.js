e4f394ca8266a9f694240948d6331b881f971197
rmEleven <1569323975@qq.com>
2024-01-25 10:55:54 +0800

    store color and auto import scss files

--- vite.config.js
+++ vite.config.js
@@ -28,9 +28,10 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        // 2. 自动导入定制化样式文件进行样式覆盖
+        // 2. 自动导入 scss 文件
         additionalData: `
           @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
         `,
       }
     }
