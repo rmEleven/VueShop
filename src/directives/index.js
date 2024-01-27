30abbc12e5fac3d588ceae9dafe814cbb4985678
rmEleven <1569323975@qq.com>
2024-01-27 13:48:01 +0800

    implement img lazy load

---
+++ src/directives/index.js
@@ -0,0 +1,26 @@
+// 懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install(app) {
+        // 指令逻辑
+        app.directive('img-lazy', {
+            mounted (el, binding) {
+                // el: 指令绑定的元素 img
+                // binding: 指令绑定的表达式的值 src
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        console.log(isIntersecting)
+                        if (isIntersecting) {
+                            // 进入视窗区域
+                            el.src = binding.value
+                            // 避免重复监听
+                            stop()
+                        }
+                    }
+                )
+            }
+        })
+    }
+}
--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
+import { lazyPlugin } from '@/directives/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
