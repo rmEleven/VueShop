1d15b92eec5d419dcb659a615ed0daa55feb5caa
rmEleven <1569323975@qq.com>
2024-01-27 16:58:17 +0800

    adapt banner

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category';
+import { getBannerAPI } from '@/apis/home'
+
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
@@ -8,10 +10,17 @@ const categoryData = ref({})
 const getCategory = async () => {
   const res = await getCategoryAPI(route.params.id)
   categoryData.value = res.result
-  console.log(categoryData.value.name)
 }
-
 onMounted(() => getCategory())
+
+const bannerList = ref([])
+const getBanner = async () => {
+    const res = await getBannerAPI({
+      distributionSite: '2'
+    })
+    bannerList.value = res.result
+}
+onMounted(() => getBanner())
 </script>
 
 <template>
@@ -23,6 +32,15 @@ onMounted(() => getCategory())
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -105,5 +123,16 @@ onMounted(() => getCategory())
   .bread-container {
     padding: 25px 0;
   }
+
+  .home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 }
 </style>
